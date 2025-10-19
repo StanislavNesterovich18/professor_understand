6ce7529ec6b5336f18104eda702cba00f050ac86
S.Nesterovich <stas.nester18@mail.ru>
2025-10-19 23:17:26 +0300

    комит добавлена функция  get_data

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_data
+
 
 if __name__ == "__main__":
     print(get_mask_card_number("2200 1209 9298 4334"))
     print(get_mask_account("7437903493 03 122 332 44342342 34"))
+
+
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(get_data("2024-03-11T02:26:18.671407"))
+
 
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_namber):
+def mask_account_card(card_namber: str) -> str:
+    """ функция которая умеет обрабатывать информацию как о картах, так и о счетах."""
     if card_namber[-20:].isdigit():
         return f"Счет {get_mask_account(card_namber[-20:])}"
-
-    #if "Cчет" in card_namber:
-       #return f"Счет {get_mask_account(card_namber[-20:])}"
     else:
         return f"{card_namber[:-16]} {get_mask_card_number(card_namber[-16:])}"
 
 
-
-
-if __name__ == '__main__':
-    print(mask_account_card("Maestro 1596837868705199"))
-    print(mask_account_card("Счет 64686473678894779589"))
+def get_data(time_card: str) -> str:
+    """принимает системную дату/время, возвращает дату в формате ДД.ММ.ГГГГ"""
+    return time_card[8:10] + "." + time_card[5:7] + "." + time_card[:4]
